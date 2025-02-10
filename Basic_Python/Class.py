e2ef57b53bc587cfd80fac76ee37e3cb1bffe385
Naveen_Jeeva <babu91872@gmail.com>
2025-02-10 23:43:31 +0530

    Function v1 learned

--- Basic_Python/Class.py
+++ Basic_Python/Class.py
@@ -1,14 +1,43 @@
-class computer:
 
-    def Config(self):
-        print("Good System")
+import arcpy
+import pandas as pd
 
-    def main(self):
-        print("Bad System")
+arcpy.env.workspace = r"D:\\GIS_Developer_Learnings\\Python\\Wales_UK.gdb"
+arcpy.env.overwriteOutput = True
+
+data =[]
+def list_field(target_field):
+    
+    fc = arcpy.ListFeatureClasses()
+    for Feature in fc:
+        field = arcpy.ListFields(Feature)
+        for field1 in field:
+            print(field1.name)
+            f1 =field1.name
+            if f1 in target_field:
+                dbf(Feature,f1)
+def dbf(Feature,f1):
+    uni_val ={}
+    with arcpy.da.SearchCursor(Feature,[f1]) as cur:
+        for val in cur:
+            value =val[0]
+            uni_val[value] = uni_val.get(value,0)+1
+    
+    for value, count in uni_val.items():
+                
+                #print(f"{str(value):<20} {str(count):<20}")
+                data.append([
+                arcpy.env.workspace,
+                Feature,
+                f1,
+                value, 
+                count
+                ])
+list_field(['fclass','type'])
+
+df = pd.DataFrame(data,columns=["Gdbpath","Feature_class","Field","Unique_Value","Count"])
+df.to_csv("D:\\books\\Trial_1_op.csv",index=False)
+#print("done")
 
 
-com1=computer()
-a = com1.Config()
-b = com1.main()
 
-print(f"The system condition is {b}")
