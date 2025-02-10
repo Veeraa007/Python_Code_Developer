f585e727ae4dff38942eed5910f54a286e1571a3
Naveen_Jeeva <babu91872@gmail.com>
2025-02-10 20:58:41 +0530

    Training class

---
+++ Basic_Python/Class.py
@@ -0,0 +1,14 @@
+class computer:
+
+    def Config(self):
+        print("Good System")
+
+    def main(self):
+        print("Bad System")
+
+
+com1=computer()
+a = com1.Config()
+b = com1.main()
+
+print(f"The system condition is {b}")
